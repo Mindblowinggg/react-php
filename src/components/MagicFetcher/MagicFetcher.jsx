4236cecf984560488790ad278dcd29f9e0d4f037
Mindblowinggg <akahar36@gmail.com>
2025-11-06 13:54:31 +0530

    as

--- src/components/MagicFetcher/MagicFetcher.jsx
+++ src/components/MagicFetcher/MagicFetcher.jsx
@@ -1,4 +1,4 @@
-import React, { use, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "../MagicFetcher/MagicFetcher.module.css";
 
 const MagicFetcher = () => {
