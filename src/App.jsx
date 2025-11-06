cc7b9250c60c948ff4d7483498e40428ea540d32
Mindblowinggg <akahar36@gmail.com>
2025-11-06 13:50:26 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import MagicFetcher from "./components/MagicFetcher/MagicFetcher";
+
+const App = () => {
+  return (
+    <div>
+      <MagicFetcher />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/MagicFetcher/MagicFetcher.jsx
@@ -0,0 +1,48 @@
+import React, { use, useEffect, useState } from "react";
+import styles from "../MagicFetcher/MagicFetcher.module.css";
+
+const MagicFetcher = () => {
+  const [message, setMessage] = useState("Waiting for magic...");
+  const [items, setItems] = useState([]);
+  const [status, setStatus] = useState();
+  const [timestamp, setTimestamp] = useState();
+
+  useEffect(() => {
+    const API_URL = "http://localhost/Magic_api.php";
+
+    fetch(API_URL)
+      .then((response) => response.json())
+      .then((data) => {
+        setMessage(data.message);
+        setItems(data.list);
+        setStatus(data.status);
+        setTimestamp(data.timestamp);
+        console.log("Magic Sucsess!");
+      })
+
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+        setMessage("oops! connection error");
+      });
+  }, []);
+  return (
+    <div className={styles.magic}>
+      <h1 className="font-bold p-10">Magic Data Viewer</h1>
+      <p>
+        Status: {status} <br /> <span>{message}</span>
+      </p>
+
+      {items.length > 0 && (
+        <ul>
+          {items.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
+      )}
+      <p>Data Backend (PHP) से आया और Frontend (React) में दिखा! 🎉</p>
+      <h1>Time: {timestamp}</h1>
+    </div>
+  );
+};
+
+export default MagicFetcher;
